d10a813093494ed0b74f3919c8f22e81df24beeb
OlegZhdanov <stalker08@gmail.com>
2020-12-03 16:49:47 +0500

    create template base.html and reformat index.html

--- geekshop/mainapp/views.py
+++ geekshop/mainapp/views.py
@@ -2,7 +2,10 @@ from django.shortcuts import render
 
 
 def index(request):
-    return render(request, template_name='mainapp/index.html')
+    context = {'title': 'Geekshop',
+               'content': 'Новые образы и лучшие бренды на GeekShop Store.\
+                    Бесплатная доставка по всему миру! Аутлет: до -70% Собственный бренд. -20% новым покупателям.'}
+    return render(request, template_name='mainapp/index.html', context=context)
 
 
 def products(request):
