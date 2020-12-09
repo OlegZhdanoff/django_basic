c0bf6409a6c184e656f267cf4f98e7b5629c4d71
OlegZhdanov <stalker08@gmail.com>
2020-12-09 08:58:21 +0500

    fix from comments

--- geekshop/mainapp/services.py
+++ geekshop/mainapp/services.py
@@ -4,7 +4,7 @@ import os
 
 
 def load_content_from_file(file):
-    with open(os.path.join(settings.CONTENTFILES_DIR, file), 'rt', encoding='utf-8') as content_file:
+    with open(os.path.join(settings.BASE_DIR, 'mainapp', 'content', file), 'rt', encoding='utf-8') as content_file:
         content_json = json.load(content_file)
 
     return content_json
--- geekshop/mainapp/views.py
+++ geekshop/mainapp/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from .services import *
+from mainapp.services import load_content_from_file
 
 
 def index(request):
