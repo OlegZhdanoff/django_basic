77396ccd0927dc4002030f00c59fe4bd944bbaf0
OlegZhdanov <stalker08@gmail.com>
2020-12-11 14:47:26 +0500

    create admin classes for models

--- geekshop/mainapp/admin.py
+++ geekshop/mainapp/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from mainapp.models import ProductCategory, Products
 
+
+class ProductCategoryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'short_description')
+    list_display_links = ('id', 'title')
+    search_fields = ('title',)
+
+
+class ProductsAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'category')
+    list_display_links = ('id', 'name')
+    search_fields = ('name', 'category')
+    list_filter = ('category',)
+
+
+admin.site.register(ProductCategory, ProductCategoryAdmin)
+admin.site.register(Products, ProductsAdmin)
 
