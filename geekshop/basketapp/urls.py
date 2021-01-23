845c5b1631893412872604d366c987e8868a5a6a
OlegZhdanov <stalker08@gmail.com>
2021-01-23 16:00:03 +0500

    BasketRemove changed to CBV

--- geekshop/basketapp/urls.py
+++ geekshop/basketapp/urls.py
@@ -6,6 +6,7 @@ app_name = 'basketapp'
 
 urlpatterns = [
     path('add/<int:id>/', basketapp.basket_add, name='basket_add'),
-    path('remove/<int:id>/', basketapp.basket_remove, name='basket_remove'),
+    # path('remove/<int:id>/', basketapp.basket_remove, name='basket_remove'),
+    path('remove/<pk>/', basketapp.BasketRemove.as_view(), name='basket_remove'),
     path('edit/<int:id>/<int:quantity>/', basketapp.basket_edit, name='basket_edit'),
 ]
--- geekshop/basketapp/views.py
+++ geekshop/basketapp/views.py
@@ -1,7 +1,10 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import HttpResponseRedirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from django.template.loader import render_to_string
 from django.http import JsonResponse
+from django.urls import reverse_lazy
+from django.views.generic import DeleteView, CreateView
 
 from mainapp.models import Products
 from basketapp.models import Basket
@@ -21,11 +24,29 @@ def basket_add(request, id=None):
     return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
 
 
-@login_required
-def basket_remove(request, id=None):
-    basket = get_object_or_404(Basket, id=id)
-    basket.delete()
-    return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
+# class BasketAdd(LoginRequiredMixin, CreateView):
+#     model = Basket
+#     # success_url =
+#
+#     def get_success_url(self):
+#         return self.request.META.get('HTTP_REFERER')
+
+# @login_required
+# def basket_remove(request, id=None):
+#     basket = get_object_or_404(Basket, id=id)
+#     basket.delete()
+#     return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
+
+
+# испльзуем миксин LoginRequiredMixin - то же что и декоратор @login_required
+class BasketRemove(LoginRequiredMixin, DeleteView):
+    model = Basket
+    success_url = reverse_lazy('auth:profile')
+    login_url = reverse_lazy('auth:login')
+
+    # чтобы не было перехода на страницу с подтверждением удаления, переопределяем метод get на post
+    def get(self, request, *args, **kwargs):
+        return self.post(request, *args, **kwargs)
 
 
 @login_required
