99eef3ebe5c2de5cff21241ba6eb55b5c37db8c6
OlegZhdanov <stalker08@gmail.com>
2021-02-02 13:43:27 +0500

    login CBV -> func

--- geekshop/authapp/urls.py
+++ geekshop/authapp/urls.py
@@ -4,7 +4,8 @@ import authapp.views as authapp
 app_name = 'authapp'
 
 urlpatterns = [
-    path('login/', authapp.UserLoginView.as_view(), name='login'),
+    # path('login/', authapp.UserLoginView.as_view(), name='login'),
+    path('login/', authapp.login, name='login'),
     path('logout/', authapp.logout, name='logout'),
     path('edit/', authapp.edit, name='edit'),
     path('register/', authapp.UserRegisterView.as_view(), name='register'),
