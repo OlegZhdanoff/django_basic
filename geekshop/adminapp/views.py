590b27068d89490ae48c85ed101743480037cd09
OlegZhdanov <stalker08@gmail.com>
2021-02-10 13:23:25 +0500

    update Products status over ProductCategory status

--- geekshop/adminapp/views.py
+++ geekshop/adminapp/views.py
@@ -1,3 +1,6 @@
+from django.db import connection
+from django.db.models.signals import pre_save
+from django.dispatch import receiver
 from django.shortcuts import render, HttpResponseRedirect
 # from django.urls import reverse
 from django.contrib.auth.decorators import user_passes_test
@@ -186,3 +189,20 @@ class CategoryDeleteView(DeleteView, UserPassesTest):
         self.object.is_visible = False
         self.object.save()
         return HttpResponseRedirect(self.get_success_url())
+
+
+def db_profile_by_type(prefix, type, queries):
+    update_queries = list(filter(lambda x: type in x['sql'], queries))
+    print(f'db_profile {type} for {prefix}:')
+    [print(query['sql']) for query in update_queries]
+
+
+@receiver(pre_save, sender=ProductCategory)
+def product_is_active_update_productcategory_save(sender, instance, **kwargs):
+    if instance.pk:
+        if instance.is_active:
+            instance.products_set.update(is_active=True)
+        else:
+            instance.products_set.update(is_active=False)
+
+        db_profile_by_type(sender, 'UPDATE', connection.queries)
