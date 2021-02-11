34a67070eca248adb4fe51772d63f843c68121e6
OlegZhdanov <stalker08@gmail.com>
2021-02-11 12:48:56 +0500

    test user login

--- geekshop/authapp/tests.py
+++ geekshop/authapp/tests.py
@@ -1,3 +1,19 @@
-from django.test import TestCase
+from datetime import datetime
 
-# Create your tests here.
+from django.test import TestCase, Client
+
+from authapp.models import ShopUser
+
+
+class TestUserAuthTestCase(TestCase):
+
+    def setUp(self):
+        self.admin = ShopUser.objects.create_superuser('admin2', 'admin2@mail.com', '123',
+                                                       birthday=datetime.date(1980, 6, 9))
+        self.client = Client()
+
+    def test_user_login(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+        self.assertTrue(response.context['user'].is_anonymous)
+        self.assertNotContains(response, 'Пользователь')
