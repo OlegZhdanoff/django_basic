52242f29c65062bc540b984cbe14a08a2d850899
OlegZhdanov <stalker08@gmail.com>
2021-02-11 12:59:46 +0500

    test user login

--- geekshop/authapp/tests.py
+++ geekshop/authapp/tests.py
@@ -6,9 +6,12 @@ from authapp.models import ShopUser
 
 
 class TestUserAuthTestCase(TestCase):
+    username = 'admin2'
+    email = 'admin2@mail.com'
+    password = '123'
 
     def setUp(self):
-        self.admin = ShopUser.objects.create_superuser('admin2', 'admin2@mail.com', '123',
+        self.admin = ShopUser.objects.create_superuser(self.username, self.email, self.password,
                                                        birthday=datetime.date(1980, 6, 9))
         self.client = Client()
 
@@ -17,3 +20,15 @@ class TestUserAuthTestCase(TestCase):
         self.assertEqual(response.status_code, 200)
         self.assertTrue(response.context['user'].is_anonymous)
         self.assertNotContains(response, 'Пользователь')
+
+        self.client.login(username=self.username, password=self.password)
+
+        response = self.client.get('/auth/login/')
+        self.assertFalse(response.context['user'].is_anonymous)
+        self.assertEqual(response.context['user'], self.admin)
+
+        response = self.client.get('/')
+        self.assertFalse(response.context['user'].is_anonymous)
+        self.assertContains(response.context['user'], self.admin)
+
+
